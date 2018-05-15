74ff116a923950e29fac8a84eb2bfcefb0b59e89
sammarcus <sam.marcus@me.com>
2018-05-15 13:36:23 -0400

    added solution

--- ruby/trip.rb
+++ ruby/trip.rb
@@ -1,1 +1,14 @@
-#Code here!
+puts "Let's plan your trip!"
+puts "Where would you like to stay?"
+
+stay = gets.chomp.downcase
+
+puts "What would you like to eat?"
+
+eat = gets.chomp
+
+puts "How many nights would you like to stay?"
+
+nights = gets.chomp
+
+puts "In your trip, you are going to stay at #{stay.upcase} for #{nights} and eat #{eat}."
