f6c152c5facd4acdab0836950d5cf54afe8614e8
sammarcus <sam.marcus@me.com>
2018-05-15 16:16:46 -0400

    removed solution

--- trip.rb
+++ trip.rb
@@ -1,13 +1,1 @@
-puts "Let's plan your trip! Where would you like to stay?"
-stay = gets.chomp.capitalize
-
-
-puts "What would you like to eat?"
-eat = gets.chomp
-
-
-puts "How many nights would you like to stay?"
-nights = gets.chomp
-
-
-puts "In your trip, you are going to stay at #{stay.capitalize} for #{nights} and eat #{eat}."
+# Code your prompts here!
